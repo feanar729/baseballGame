32cffdf9f05e1ddc8949da30bdef520f27a401dc
feanar729 <feanar729@gmail.com>
2018-02-28 22:16:19 +0900

    remake function baseballGame.js second

--- baseballGame.js
+++ baseballGame.js
@@ -1,18 +1,18 @@
-// var input = prompt("숫자를 쓰시오");
 var input = "321";
 var strike = 0;
 var ball = 0;
 
-var inputNumber = function(inputMakeArray) {
+// 번호 넣는 함수
+function inputNumber(inputMakeArray) {
   if (inputMakeArray === "" || inputMakeArray === null || inputMakeArray === undefined) {
     console.log("빈 값 혹은 입력 오류!");
   } else {
     return inputMakeArray.split("");
   }
-};
+}
 
 // 랜덤번호 생성함수
-var makeNumber = function() {
+var makeNumber = function () {
   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var number = [];
 
@@ -23,54 +23,38 @@ var makeNumber = function() {
   return number;
 };
 
-// Todo 계획
-// 1. 입력값(inputNumber)과 생성값(makeNumber)의 비교
-// 2. 비교 함수에서 결론을 출력하는 함수
-// 3. 출력된 함수가 strike 3개시 초기화 시켜주는 기능 넣기.(함수? 출력함수내에 조건문? 고민..)
+// 체크된 strike, ball을 출력하는 함수
+var resultAnswer = function (strike, ball, inputNumber) {
 
-// 초기화 함수[수정중]
-var initialize = function (strike, ball) {
-  strike = 0;
-  ball = 0;
-  while (strike <= 3) {
-    makeNumber();
+  if (strike === 3) {
+    console.log("3 스트라이크" + '\n' + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  } else if (strike == 0 && ball == 0) {
+    console.log("낫싱!!");
+  } else {
+    console.log(inputNumber.join("") + ": " + strike + "스트라이크 " + ball + "볼");
   }
 };
 
-// 비교함수[수정중]
-var checkNumber = function(inputNumberArray) {
-  var inputArray = inputNumber(inputNumberArray);
-  var number = makeNumber();
-  // 위에 변수 2개의 데이터는 무사히 잘 나옴 console,log(inputArray, number); => ["3", "2", "1"], [랜덤 생성값] 
+// 비교함수[추가: strike를 3번될때까지 어떻게 반복으로 돌릴것인가? ]
+// 랜덤번호의 값을 갖고 있는 상태에서 input값을 비교만 하게 하는 방법.
+var checkNumber = function (inputNum) {
+  var number = inputNumber(inputNum);
+  var makeArray = makeNumber();
+  strike = 0;
+  ball = 0;
+
   for (var i = 0; i < 3; i++) {
     for (var k = 0; k < 3; k++) {
-      if (number[i] == inputArray[k]) {
+      if (number[i] == makeArray[k]) {
         if (i === k) {
-          return strike++;
+          strike++;
         } else {
-          return ball++;
+          ball++;
         }
       }
     }
   }
-};
-
-
-// 비교된 값의 출력함수
-var resultNumber = function (inputNum) {
-  var strike = checkNumber();
-  var ball = checkNumber();
-  var inputArray = inputNumber(inputNum);
-
-  if (strike === 3) {
-    console.log("3 스트라이크" + "\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-  } else if (strike === 0 && ball === 0) {
-    console.log("낫싱!!");
-  } else {
-    console.log(inputArray.join("") + ": " + strike + "스트라이크 " + ball + "볼");
-  }
+  resultAnswer(strike, ball, number);
 };
 
 checkNumber(input);
-// checkNumber(input));
-
